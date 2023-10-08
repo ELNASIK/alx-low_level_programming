e60689391922f9c8368544ad4cedecca1b2cfd70
ELNASIK <elnasikmohana@gmail.com>
2023-10-08 15:13:30 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+/**
+ * main - this is the main function
+ * return : always 0
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-return (0); }	
+	puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}	
