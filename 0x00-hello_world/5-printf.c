f771e82cae3cf76a9498b560e57f9132aa4637aa
ELNASIK <elnasikmohana@gmail.com>
2023-10-09 11:26:10 -0700

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A c programme that prints with function printf
+ *
+ * Return: Always 0 (sucsess)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
