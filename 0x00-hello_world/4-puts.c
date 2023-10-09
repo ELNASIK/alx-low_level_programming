bb740409b9198ea16db0cd8103345d5fdb20ff7e
ELNASIK <elnasikmohana@gmail.com>
2023-10-09 09:56:08 -0700

    printing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ *main - Entry point
  *
  *Description: c programme that print with put function
  *
- * return: always 0 (success)
+ *return: always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }	
