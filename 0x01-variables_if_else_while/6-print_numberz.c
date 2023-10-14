52fda1f223634faf3c1b625e2fcb86d8292980ce
ELNASIK <elnasikmohana@gmail.com>
2023-10-14 14:44:05 -0700

    conditioning and looping

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: ' a program that prints all single digit numbers of base 10'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		putchar(n + 48);
+		n++;
+	}
+	printf("\n");
+
+	return (0);
+}
