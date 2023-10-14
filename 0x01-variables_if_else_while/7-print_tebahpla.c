f4e711a7a35cc1d7bc828f6676c3114ea1cdf5ae
ELNASIK <elnasikmohana@gmail.com>
2023-10-14 14:55:00 -0700

    conditioning and looping

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'a program that prints the lowercase alphabet in reverse'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'z' :
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	printf("\n");
+
+	return (0);
+}
