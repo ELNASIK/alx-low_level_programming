7382f895c2db7aa7ce437d4ff0ebcb38384196ff
ELNASIK <elnasikmohana@gmail.com>
2023-10-14 14:19:41 -0700

    conditioning and looping

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	int n = 0;
+	while (n <= 9)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+
+	return (0);
+}	
