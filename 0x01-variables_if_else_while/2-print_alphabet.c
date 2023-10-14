4d233b8834acee3f01af121c6b27ed3c55c03555
ELNASIK <elnasikmohana@gmail.com>
2023-10-14 12:53:59 -0700

    conditioning and looping

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,11 +10,13 @@ int main(void)
 
 {
 	char l = 'a';
+
 	while (l <= 'z')
 	{
 		putchar(l);
-    		l++;
-        }
-        putchar('\n');
-        return (0);
+		l++;
+	}
+	putchar('\n');
+
+	return (0);
 }
