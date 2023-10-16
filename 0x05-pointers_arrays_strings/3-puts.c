50521c5dae5c693336290d3d4ee3259e34bcfa5e
ELNASIK <elnasikmohana@gmail.com>
2023-10-16 16:57:15 -0700

    pointers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,9 +3,12 @@
  * _puts - prints a string, followed by a new line
  *
  * @str: string parameter output
+ * @c: The character to print
  *
  * Return: Nothing
+ *
  */
+int _putchar(char c);
 void _puts(char *str)
 {
 	while (*str != '\0')
