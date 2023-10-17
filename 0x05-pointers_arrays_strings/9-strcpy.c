7307800f3c92de75ce0209ca1a80fc4ad0222e0f
ELNASIK <elnasikmohana@gmail.com>
2023-10-17 14:57:08 -0700

    pointers git commit -mpointers

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,13 +9,12 @@
  * Return: pointer to 'dest'
 */
 char *_strcpy(char *dest, char *src)
-
 {
 	int i = -1;
 
 	do {
 		i++;
-		dest[i] = srrc[];
+		dest[i] = src[i];
 	} while (src[i] != '\0');
 
 		return (dest);
