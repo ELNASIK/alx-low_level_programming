7c9cc30a257b5b8a1647852f1f3e08eeca9d62b3
ELNASIK <elnasikmohana@gmail.com>
2023-10-16 17:42:40 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ *
+ * @s:reversed string  parameter to print
+ *
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c])
+
+		c++;
+
+	while (s[c])
+	{
+		c--;
+		putchar(s[c]);
+	}
+	putchar('\n';)
+}
