833e18ff48161dcdeb3f78982870212cd410e0f2
ELNASIK <elnasikmohana@gmail.com>
2023-10-19 15:40:31 -0700

    more pointers

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -23,9 +23,32 @@ int isDelimiter(char c)
 	char delimiter[] = " \t\n,.!?\"(){}";
 
 	for (i = 0 ; i < 12; i++)
-	{
 		if (c == delimiter[i])
 			return (1);
-	}
 	return (0);
+}
+/**
+ * cap_string -  function that capitalizes all words of a string.
+ * @str: input a string
+ *
+ * Return: string with capitalized words
+ *
+*/
+
+char *cap_string(char *str)
+{
+	char *ptr = str;
+	int foundDelimit = 1;
+
+	while (*str)
+	{
+		if (isDelimiter(str))
+			foundDelimit = 1;
+		else if (isLower(*str) &&  foundDelimit)
+		{
+			*str -= 32;
+			 foundDelimit = 0;
+		}
+		str++;
+	}
 }
