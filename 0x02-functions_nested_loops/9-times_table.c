3d87e2bfef87515ed91f2811f7f97ed4c9e02ca2
ELNASIK <elnasikmohana@gmail.com>
2023-10-21 15:19:49 -0700

    t

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,11 +8,11 @@ void times_table(void)
 {
 	int n, l;
 
-	for (n = 48; n <= 57; n++)
+	for (n = 0; n <= 0; n++)
 	{
-		for (l =48; l <= 57; l++)
+		for (l =0; l <= 9; l++)
 		{
-			_putchar(n * l);
+			_putchar((n * l ) + 48);
 			_putchar(44);
 			_putchar(32);
 		}
