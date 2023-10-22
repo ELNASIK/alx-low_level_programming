ed5a9a937c675a3b9c9d488aa45608b901aa6a04
ELNASIK <elnasikmohana@gmail.com>
2023-10-22 15:09:13 -0700

    x

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar(43);
+		_putchar(43);
 		return (1);
 	}
 	if (n == 0)
 	{
-		putchar(43);
+		_putchar(43);
 		return (0);
 	}
 	else
 	{
-		putchar(45);
+		_putchar(45);
 		return (-1);
 	}
 }
