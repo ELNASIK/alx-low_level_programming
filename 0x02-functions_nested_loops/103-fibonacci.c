c209b6ed307a723474fe86e60f6d2ea4bc63ca77
ELNASIK <elnasikmohana@gmail.com>
2023-10-22 14:33:40 -0700

    c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,13 +11,13 @@ int main(void)
         int i;
         unsigned long f1 = 0, f2 = 1, sum;
 
-        for (i = 0; i < 70; i++)
+        for (i = 0; i < 55; i++)
         {
                 sum = f1 + f2;
                 printf("%lu", sum);
                 f1 = f2;
                 f2 = sum;
-                if (i == 69)
+                if (i == 54)
                         printf("\n");
                 else
                         printf(", ");
