cb22a2776e334ff02731067a213e3051fc1e855b
ELNASIK <elnasikmohana@gmail.com>
2023-10-22 06:35:40 -0700

    win

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_times_table - prints the 9 times table, starting with 0.
+ * @n: number of time tabels
+ * Return: Always 0 (success)
+*/
+void print_times_table(int n)
+{
+	int  l, mul_1, i, mul;
+
+	if ((n < 15) && (n > 0))
+	{
+		for (l = 0; l <= n; l++)
+		{
+			for (i = 0; i < n; i++)
+			{
+				mul_1 = i * l;
+				_putchar(mul_1);
+				_putchar(',');
+				_putchar(32);
+			}
+			mul = n * l;
+			_putchar(mul);
+		}
+	}
+	_putchar('\n');
+}
