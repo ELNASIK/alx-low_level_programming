b4eb78f70ec588c064ee05bec807049619ecd98e
ELNASIK <elnasikmohana@gmail.com>
2023-10-24 15:20:11 -0700

    last

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,26 +1,18 @@
+
 #include "main.h"
-#include <stdio.h>
+
 /**
- * print_diagsums -  a function that sets the value of a pointer to a char.
- * @a: input parmeter
- * @size: input parameter
- * Return: Always 0 (Success)
- */
-void print_diagsums(int *a, int size)
+  * set_string - a function that sets the value of a pointer to a char.
+  *
+  * @s: input  parameter
+  *
+  * @to: input  parameter
+  *
+  * Return: Always 0 (success).
+  */
+void set_string(char **s, char *to)
+
 {
-	int s1, s2, i;
+	*s = to;
 
-	s1 = 0;
-	s2 = 0;
-
-	for (i = 0; i < size; i++)
-	{
-		s1 = s1 + a[i * size + i];
-	}
-	for (i = size - 1; i >= 0; i--)
-	{
-		s2 += a[i * size + (size - i - 1)];
-	}
-	printf("%d, %d\n", s1, s2);
 }
-
