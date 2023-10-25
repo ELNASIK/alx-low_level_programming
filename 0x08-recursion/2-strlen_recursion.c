a2d5a3ecd49eaf8f168f91e6841097969db1b9b2
ELNASIK <elnasikmohana@gmail.com>
2023-10-25 14:58:14 -0700

    revers

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion -a function that returns the length of a string.
+ * @s:sting to be scanned
+ *
+ * Return:length of the input string
+ *
+*/
+int _strlen_recursion(char *s)
+{
+	int i = 1;
+
+	if (*s != '\0')
+	{
+		return (i += _strlen_recursion(s + 1));
+	}
+}
