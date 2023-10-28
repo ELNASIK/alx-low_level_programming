51e4375d47053f8aa2162b3126fdd6eed5ac8592
ELNASIK <elnasikmohana@gmail.com>
2023-10-28 05:02:02 -0700

    arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main  - Entry point
+ * @argv: arry of pointers
+ * @argc: number of string arguments in argv
+ * Return:0 (success)
+ *
+*/
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
