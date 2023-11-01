75b602554166a062d56266c017610c516d835e4e
ELNASIK <elnasikmohana@gmail.com>
2023-11-01 10:36:33 -0700

    m

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,7 +18,7 @@ char *str_concat(char *s1, char *s2)
 	for (j = 0; s2[j] != '\0'; j++)
 		;
 	c = malloc(i + j + 1);
-	for (n = 0; n <= i; n++)
+	for (n = 0; n < i; n++)
 		c[n] = s1[n];
 	for (n = i; n <= (i + j); n++)
 		c[n] = s2[n];
